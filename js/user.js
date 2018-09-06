a99c0e456d9506eaed8c11c78c7b9b5550edd77b
mskrabka <marta.skrabka@dataart.com>
2018-09-06 13:02:33 +0200

    new website

---
+++ js/user.js
@@ -0,0 +1,28 @@
+   $(document).ready(function(){
+   $(window).on('load', function() {
+$('h1').show(2000);
+});
+    
+});
+
+$(document).ready(function(){
+    $(".apple").mouseenter(function(){
+        $("#news").css("background-color", "lightblue");
+    });
+    $(".apple").mouseleave(function(){
+        $("#news").css("background-color", "lightpink");
+    });
+});
+
+$(document).ready(function(){
+    $(".more_info").click(function(){
+        $(".more_text").show(1000);
+        $(".less_text").show(1000);
+        $(".more_info").hide(1000);
+    });
+    $(".less_text").click(function(){
+        $(".more_text").hide(1000);
+        $(".less_text").hide(1000);
+        $(".more_info").show(1000);
+    });
+});
